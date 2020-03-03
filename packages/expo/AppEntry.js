152d7380a494986ff51f4a1d65401576aa635a9e
samyoungnyc <sambyoung@gmail.com>
2020-03-03 13:04:16 -0500

    Filepath fix
    Changing the filepath to src/App

--- packages/expo/AppEntry.js
+++ packages/expo/AppEntry.js
@@ -2,7 +2,7 @@ import 'expo/build/Expo.fx';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import { activateKeepAwake } from 'expo-keep-awake';
 
-import App from '../../App';
+import App from '../../src/App';
 
 if (__DEV__) {
   activateKeepAwake();
